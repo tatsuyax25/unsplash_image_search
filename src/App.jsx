aa3842b8336ba5a53a2cc795a6f9edddc822845e
Miguel Urena <urena_miguel82@outlook.com>
2023-10-04 17:33:54 -0700

    Replace content on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { Form } from "react-bootstrap";
 import './index.css';
 
 const App = () => {
-  return <div>Welcome to Unsplash Image Search</div>
+  return (
+    <div className="container">
+      <h1 className="title">Image Search</h1>
+      <div className="search-section">
+        <Form>
+          <Form.Control type="search" placeholder="Type something to search..." className="search-input" />
+        </Form>
+      </div>  
+    </div>
+  )
 };
 
 export default App;
