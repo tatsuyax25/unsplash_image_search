f188d71ca7e72df1a2ba26afae06836ea19b4291
Miguel Urena <urena_miguel82@outlook.com>
2023-10-04 17:37:08 -0700

    Add Search Input

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Form } from "react-bootstrap";
 import './index.css';
 
 const App = () => {
+  const searchInput = useRef(null);
   return (
     <div className="container">
       <h1 className="title">Image Search</h1>
       <div className="search-section">
         <Form>
-          <Form.Control type="search" placeholder="Type something to search..." className="search-input" />
+          <Form.Control type="search" placeholder="Type something to search..." className="search-input" ref={searchInput} />
         </Form>
       </div>  
     </div>
