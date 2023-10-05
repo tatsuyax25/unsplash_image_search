3b744f0da268090a3cea922977868899b20b138c
Miguel Urena <urena_miguel82@outlook.com>
2023-10-04 17:43:13 -0700

    Form submit action

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,17 @@ import './index.css';
 
 const App = () => {
   const searchInput = useRef(null);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    console.log('submitted');
+  }
+
   return (
     <div className="container">
       <h1 className="title">Image Search</h1>
       <div className="search-section">
-        <Form>
+        <Form onSubmit={handleSearch}>
           <Form.Control type="search" placeholder="Type something to search..." className="search-input" ref={searchInput} />
         </Form>
       </div>  
