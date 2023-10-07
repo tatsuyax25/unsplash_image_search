e35f096c284db24a020b5977b61ec2379bb0f6cd
Miguel Urena <urena_miguel82@outlook.com>
2023-10-07 12:35:27 -0700

    Fix bug issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,95 @@
-import axios from "axios";
-import { useEffect, useRef, useState } from "react";
-import { Button, Form } from "react-bootstrap";
+import axios from 'axios';
+import { useEffect, useRef, useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 import './index.css';
 
 const API_URL = 'https://api.unsplash.com/search/photos';
 const IMAGES_PER_PAGE = 20;
+
 const App = () => {
   const searchInput = useRef(null);
   const [images, setImages] = useState([]);
-  const [totalPages, setTotalPages] = useState(0);
   const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
 
   useEffect(() => {
     fetchImages();
   }, [page]);
 
   const fetchImages = async () => {
-  try {
-    const { data } = await axios.get(
-      `${API_URL}?query=${
-        searchInput.current.value
-      }&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${
-        import.meta.env.VITE_API_KEY
-      }`
-    );
-    console.log('data', data);
-    setImages(data.results);
-    setTotalPages(data.total_pages);
-  } catch (error) {
-    console.log(error);
-  }
-};
+    try {
+      const { data } = await axios.get(
+        `${API_URL}?query=${
+          searchInput.current.value
+        }&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${
+          import.meta.env.VITE_API_KEY
+        }`
+      );
+      console.log('data', data);
+      setImages(data.results);
+      setTotalPages(data.total_pages);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-const resetSearch = () => {
-  setPage(1); // reset page
-  fetchImages();
-}
-const handleSearch = (e) => {
-    e.preventDefault();
+  const resetSearch = () => {
+    setPage(1);
+    fetchImages();
+  };
+
+  const handleSearch = (event) => {
+    event.preventDefault();
     console.log(searchInput.current.value);
-    resetSearch(); // reset search
+    resetSearch();
   };
 
   const handleSelection = (selection) => {
     searchInput.current.value = selection;
-    resetSearch(); // reset search
+    resetSearch();
   };
 
   console.log('page', page);
 
   return (
-    <div className="container">
-      <h1 className="title">Image Search</h1>
-      <div className="search-section">
+    <div className='container'>
+      <h1 className='title'>Image Search</h1>
+      <div className='search-section'>
         <Form onSubmit={handleSearch}>
-          <Form.Control 
-            type="search" 
-            placeholder="Type something to search..." 
-            className="search-input" 
-            ref={searchInput} 
+          <Form.Control
+            type='search'
+            placeholder='Type something to search...'
+            className='search-input'
+            ref={searchInput}
           />
         </Form>
       </div>
-      <div className="filters">
+      <div className='filters'>
         <div onClick={() => handleSelection('nature')}>Nature</div>
-        <div onClick={() => handleSelection('bird')}>Birds</div>
+        <div onClick={() => handleSelection('birds')}>Birds</div>
         <div onClick={() => handleSelection('cats')}>Cats</div>
         <div onClick={() => handleSelection('shoes')}>Shoes</div>
       </div>
       <div className='images'>
-      {images.map((image) => (
-        <img
-          key={image.id}
-          src={image.urls.small}
-          alt={image.alt_description}
-          className='image'
-        />
-      ))}
+        {images.map((image) => (
+          <img
+            key={image.id}
+            src={image.urls.small}
+            alt={image.alt_description}
+            className='image'
+          />
+        ))}
       </div>
-      <div className="buttons">
-        {page > 1 && (<Button onClick={() => setPage(page - 1)}>Previous</Button>)}
-        {page < totalPages && (<Button onClick={() => setPage(page + 1)}>Next</Button>)}
+      <div className='buttons'>
+        {page > 1 && (
+          <Button onClick={() => setPage(page - 1)}>Previous</Button>
+        )}
+        {page < totalPages && (
+          <Button onClick={() => setPage(page + 1)}>Next</Button>
+        )}
       </div>
     </div>
-  )
+  );
 };
 
 export default App;
